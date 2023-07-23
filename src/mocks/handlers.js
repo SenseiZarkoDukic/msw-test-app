e51bf3cfd5c4445215bde1e9d72645d9c9c99452
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-23 23:05:02 +0200

    Created request handlers by callin rest[METHOD] and providing a request path

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,1 +1,4 @@
 // module where we will have all our request handlers. The handlers from this module can be reused for both browser and Node
+import { rest } from "msw";
+
+export const handlers = [rest.post("/login", null), rest.get("/user", null)];
