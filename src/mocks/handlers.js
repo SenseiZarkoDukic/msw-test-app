e25afcc4c9312bd355c15e1c78e668204ffd46f2
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-24 14:46:30 +0200

    Setting up files and folders for MSW and starting worker

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,9 +3,26 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.post("/login", (req, res, ctx) => {
-    sessionStorage.setItem("isAuthenticated", true);
+    sessionStorage.setItem("is-authenticated", "true");
+    return res(ctx.status(200));
   }),
-  return res(ctx.sta)
+  rest.get("/user", (req, res, ctx) => {
+    const isAuthenticated = sessionStorage.getItem("is-authenticated");
 
-  rest.get("/user", null),
+    if (!isAuthenticated) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: "Not authorized",
+        })
+      );
+    }
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        username: "admin",
+      })
+    );
+  }),
 ];
